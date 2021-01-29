e78ec0793785ebfda8240eec0eca5c4cd447d5bd
Diane Crame <dcrame@icloud.com>
2021-01-28 17:52:12 -0800

    add plants controller

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,15 @@
+class PlantsController < ApplicationController
+  def index
+  end
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'plants/index'
+  get 'plants/show'
+  get 'plants/create'
+  get 'plants/update'
+  get 'plants/destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
